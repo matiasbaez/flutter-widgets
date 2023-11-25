3621b130ee6d8b1a60e0217e2508ac028fa3a2cc
Matias Baez <matiasbaez2512@gmail.com>
2023-11-25 13:18:51 -0300

    Home screen ListView builder

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -10,6 +11,49 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      }
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+
+  final MenuItem menuItem;
+
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final color = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text( menuItem.title ),
+      subtitle: Text( menuItem.subtitle ),
+      leading: Icon( menuItem.icon, color: color.primary ),
+      trailing: Icon( Icons.arrow_forward_ios_rounded, color: color.primary ),
+      onTap: () {},
     );
   }
 }
